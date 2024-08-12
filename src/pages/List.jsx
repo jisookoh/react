790bc62586a58e4b0a77b54e6fe6400e4de9a454
jisookoh <gosjioo0312@naver.com>
2024-08-12 22:22:39 +0900

    테스트입니다.

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { buttonStyle } from "../style/button";
 
 export default function List() {
-  return <StyledButton>버튼</StyledButton>;
+  return <StyledButton>버튼1</StyledButton>;
 }
 
 const StyledButton = styled.button`
