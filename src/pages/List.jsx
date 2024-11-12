4f1d0d48a1af8247c14e2881ed54b93a9e1b0aee
mijin <12rlaalwls@gmail.com>
2024-11-12 15:05:30 +0900

    버튼 하나 추가 test
    버튼 하나 추가 test

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -2,10 +2,18 @@ import styled from "styled-components";
 import { buttonStyle } from "../style/button";
 
 export default function List() {
-  return <StyledButton>버튼1</StyledButton>;
+  return (
+    <button_wrap>
+      <StyledButton>버튼1</StyledButton>
+      <StyledButton2>버튼2</StyledButton2>
+    </button_wrap>
+  );
 }
 
 const StyledButton = styled.button`
   color: black;
   ${buttonStyle}
+`;
+const StyledButton2 = styled.button`
+  color: black;
 `;
